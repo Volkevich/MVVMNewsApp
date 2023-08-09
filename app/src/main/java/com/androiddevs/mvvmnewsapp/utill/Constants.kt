23ca1a8fbaf151a4905ce8141080d920c334f737
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-08-09 22:39:56 +0300

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utill/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utill
+
+class Constants {
+    companion object{
+        const val API_KEY = "2a8ac21c8f124c35ac750af245527cd3"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
