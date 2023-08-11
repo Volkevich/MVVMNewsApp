5e98d8057d4471f82161ffe30827cb4cc9a66bbc
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-08-11 21:18:21 +0300

    NewsAdapter.kt

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
@@ -1,7 +1,65 @@
 package com.androiddevs.mvvmnewsapp.ui.fragment
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.androiddevs.mvvmnewsapp.NewsActivity
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.adapter.NewsAdapter
+import com.androiddevs.mvvmnewsapp.utill.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.paginationProgressBar
+import kotlinx.android.synthetic.main.fragment_breaking_news.rvBreakingNews
 
 class BreakingNewsFragment:Fragment (R.layout.fragment_breaking_news) {
+
+
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter:NewsAdapter
+
+    val TAG = "Последние новости"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { responce->
+            when(responce){
+                is Resource.Success ->{
+                    hideProgressBar()
+                    responce.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error ->{
+                    hideProgressBar()
+                    responce.message?.let { message->
+                        Log.e(TAG,"Произошла ошибка: $message")
+                    }
+                }
+                is Resource.Loading ->{
+                    showProgressBar()
+                }
+            }
+        })
+
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
 }
